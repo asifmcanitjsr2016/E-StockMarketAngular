390715a13f15efa2a91220464d3a3c3387fddb6e
unknown <cogdotnet621@iiht.tech>
2022-07-11 18:26:20 +0530

    code cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,24 +6,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddCompanyComponent } from './add-company/add-company.component';
-import { AllCompanyComponent } from './all-company/all-company.component';
-import { SearchCompanyComponent } from './search-company/search-company.component';
 import { ErrorMessageComponent } from './error-message/error-message.component';
 import { SuccessMessageComponent } from './success-message/success-message.component';
 import { InfoMessageComponent } from './info-message/info-message.component';
 import { HttpClientModule } from '@angular/common/http';
-import { EStockTabsComponent } from './e-stock-tabs/e-stock-tabs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddCompanyComponent,
-    AllCompanyComponent,
-    SearchCompanyComponent,
+    AddCompanyComponent,    
     ErrorMessageComponent,
     SuccessMessageComponent,
-    InfoMessageComponent,
-    EStockTabsComponent
+    InfoMessageComponent,    
   ],
   imports: [
     BrowserModule,
